7a97e49e7e9c94a0ae0edbab89e89ea7c965f879
xtashi <tcharlles.us@gmail.com>
2023-08-07 21:13:45 -0300

    cloud background add

--- weather/src/components/Home.tsx
+++ weather/src/components/Home.tsx
@@ -2,7 +2,7 @@ import WeatherCard from "./WeatherCard";
 
 const Home = () => {
   return (
-    <div className="bg-blue-sky min-h-screen w-full flex items-center justify-center" >
+    <div className="bg-blue-sky min-h-screen bg-[url('./assets/cloud.svg')] bg-cover w-full flex items-center justify-center" >
       <div>
         <WeatherCard />
       </div>
